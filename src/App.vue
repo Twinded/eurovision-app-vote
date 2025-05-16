ff08a86ba00e7b990ad20d73e5ad2c84dc23c089
Twinded <perso.thomas.clr@gmail.com>
2025-05-16 10:28:21 -0400

    Revert "✨ Add vercel SpeedInsights"
    This reverts commit 4c6f72fafec679d8c717643744aa0e33d258425b.

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,8 @@
 <template>
-  <SpeedInsights>
-    <router-view></router-view>
-    <BottomNav />
-  </SpeedInsights>
+  <router-view></router-view>
+  <BottomNav />
 </template>
 
 <script setup>
 import BottomNav from '@/components/BottomNav.vue';
-import { SpeedInsights } from '@vercel/speed-insights/vue';
 </script>
